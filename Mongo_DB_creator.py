6d005fd3c45e9c5d39b80ed4da7b63a72ed24063
Marco Michellini <145436398+gammic@users.noreply.github.com>
2025-06-06 11:48:56 +0200

    Delete Mongo_DB_creator.py

--- Mongo_DB_creator.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-import json
-client=MongoClient("mongodb://localhost:27017/")
-
-db=client["amazonDB"]
-collection=db["reviews"]
-with open('Reviews.json', 'r') as f:
-    reviews = json.load(f)
-collection.insert_many(reviews)
